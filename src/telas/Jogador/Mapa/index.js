cdb985b31357e37f6619ca2bfcd0f14cd4a679d3
LarissaVassao <larissasantosvassao@gmail.com>
2025-09-06 16:14:34 -0300

    Larissa

--- src/telas/Jogador/Mapa/index.js
+++ src/telas/Jogador/Mapa/index.js
@@ -8,7 +8,8 @@ import {
   PanResponder,
   ScrollView,
   Animated,
-  TouchableOpacity,
+  TouchableOpacity, 
+  StatusBar,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; 
 
@@ -55,12 +56,22 @@ const updateTokenPosition = (id, newX, newY) => {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity 
-        style={styles.backButton}
-        onPress={() => navigation.navigate("Home")}
-        >
-          <Ionicons name="arrow-back-outline" size={30} color="#3B004F" />
-      </TouchableOpacity>
+      <StatusBar backgroundColor="#124A69" barStyle="light-content" />
+                  
+                  <View style={styles.header}>
+                      <TouchableOpacity 
+                          style={styles.backButton}
+                          onPress={() => navigation.navigate("Home")}
+                      >
+                          <Ionicons name="arrow-back-outline" size={20} color="#fff" />
+                      </TouchableOpacity>
+                      
+                      <Text style={styles.headerTitle}>MAPA</Text>
+                      
+                      
+      </View>
+      <View style={styles.he}>
+
       {!showGrid && (
         <Form
           gridWidth={gridWidth}
@@ -94,16 +105,19 @@ const updateTokenPosition = (id, newX, newY) => {
               onDrop={updateTokenPosition}
             />
           ))}
-          <TouchableOpacity style={styles.button} onPress={addToken}>
-            <Text style={styles.buttonText}>Criar Token</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => setShowGrid(false)}>
-          <Text style={styles.buttonText}>Alterar Mapa</Text>
-          </TouchableOpacity>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.button} onPress={addToken}>
+              <Text style={styles.buttonText}>Criar Token</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => setShowGrid(false)}>
+              <Text style={styles.buttonText}>Alterar Mapa</Text>
+            </TouchableOpacity>
+          </View>
           </ScrollView>
           </ScrollView>
         </>
       )}
+      </View>
     </View>
   );
 }
@@ -237,19 +251,32 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     flex: 1,
-    paddingHorizontal: 25,
+    paddingHorizontal: 0,
+    paddingTop: 0,
+  },
+  he: {
+     paddingHorizontal: 25,
     paddingTop: 20,
-  },
-  button: {
-    backgroundColor: '#124A69',
-    width: 120,
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 20,
-    borderRadius: 5,
-    alignSelf: 'center',
-  },
+    },
+buttonsContainer: {
+  flexDirection: 'row',
+  justifyContent: 'center', // Centraliza os botões
+  alignItems: 'center',
+  marginVertical: 10,
+  gap: 20, // 20px de distância entre os botões
+},
+button: {
+  backgroundColor: '#124A69',
+  width: 120,
+  height: 40,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 5,
+},
+buttonText: {
+  color: '#fff',
+  fontWeight: 'bold',
+},
   buttonPermanent:{
     backgroundColor: '#124A69',
     width: 120,
@@ -261,10 +288,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     position: 'absolute'
   },
-  buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  },
+
   formContainer: {
     padding: 20,
     alignItems: 'center',
@@ -284,5 +308,37 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
     borderRadius: 4,
     textAlign: 'center',
+  },
+    header: {
+    backgroundColor: '#124A69',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    height: 60,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+  },
+ headerTitle: {
+  color: '#fff',
+  fontSize: 20,
+  fontWeight: '600',
+  letterSpacing: 0.3,
+  textAlign: 'center',
+  flex: 1, 
+  marginHorizontal: 10,
+  top: 5
+},
+  backButton: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
