9f1f2b08fbee0c987210d0aaf127c38a95933498
mtLeme07 <mt.leme07@gmail.com>
2025-09-11 10:18:06 -0300

    added checarPersonagem.php

---
+++ rpgetec/checarPersonagem.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+include_once('conexao.php');
+
+$id_personagem = $_GET['id_personagem'] ?? '';
+
+if ($id_campanha == '') {
+    echo json_encode(['success' => false, 'error' => 'id_personagem not provided']);
+    exit;
+}
+try {
+	$queryStr = "SELECT * FROM personagem WHERE id = :id_personagem";
+    $query = $pdo->prepare($queryStr);
+    $query->bindParam(':id_personagem', $id_personagem);
+    $query->execute();
+    $res = $query->fetch(PDO::FETCH_ASSOC);
+    echo json_encode([
+        'success' => true,
+        'personagens' => $res 
+    ]);
+} catch (PDOException $e) {
+    echo json_encode([
+        'success' => false,
+        'error' => $e->getMessage()
+    ]);
+}
+?>
