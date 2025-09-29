1f7c3212b07c961a88cd21d7c31f242744f43827
LarissaVassao <124535239+LarissaVassao@users.noreply.github.com>
2025-09-29 14:20:29 -0300

    larissa esqueceu

--- src/telas/Tutorial/index.js
+++ src/telas/Tutorial/index.js
@@ -67,39 +67,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
-  header: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#fff',
-    textAlign: 'center',
-    marginVertical: 15,
-  },
-  imagem: {
-    width: '100%',
-    height: 250,
-    borderRadius: 8,
-    resizeMode: 'cover',
-  },
-  loading: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000',
-  },
-  loadingText: {
-    marginTop: 10,
-    fontSize: 16,
-    color: '#fff',
-  },
-  emptyText: {
-    textAlign: 'center',
-    marginTop: 20,
-    fontSize: 16,
-    color: '#fff',
-  },
-});
